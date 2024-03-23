97b8183053e6c2c0365ec2b78e6b0c997b01f4f8
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-03-23 17:04:20 +0700

    delete junk files

--- NganHangNhaTro_G20/Controllers/HomeController.cs
+++ NganHangNhaTro_G20/Controllers/HomeController.cs
@@ -17,22 +17,7 @@ namespace NganHangNhaTro_G20.Controllers
         {
             return View();
         }
-        public IActionResult Login()
-        {
-            return View();
-        }
-        public IActionResult Register()
-        {
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult HouseDetail()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
