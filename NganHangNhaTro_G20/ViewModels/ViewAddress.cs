af6f5e3b2dd09467b67fc8319c19d471e60d5aeb
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-03-28 10:07:18 +0700

    clone data home

---
+++ NganHangNhaTro_G20/ViewModels/ViewAddress.cs
@@ -0,0 +1,12 @@
+﻿using NganHangNhaTro_G20.Models;
+
+
+namespace NganHangNhaTro_G20.ViewModels
+{
+    public class ViewAddress
+    {
+        public List<Location> Locations { get; set; }
+        public List<House> Houses { get; set; }
+
+    }
+}
