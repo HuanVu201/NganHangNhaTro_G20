2c7316b852b1c67fa90595f30a0c103d91671a24
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-04-06 23:29:43 +0700

    info user and list bookin

---
+++ NganHangNhaTro_G20/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NganHangNhaTro_G20.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        public IActionResult UserInfo()
+        { 
+            return View();
+        }
+        public IActionResult UserInfoHouseBooking()
+        {
+            return View();
+        }
+    }
+}
