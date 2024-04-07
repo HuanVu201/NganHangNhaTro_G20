90d692a5105dbfa8323b5dbd83c1bad59fd33641
Dat Nguyen <ntdat09mister@gmail.com>
2024-04-07 12:59:26 +0700

    update logic user info bookingcalender

---
+++ NganHangNhaTro_G20/ViewModels/UserInfoHouseBookingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NganHangNhaTro_G20.Models;
+using NganHangNhaTro_G20.ViewModels;
+
+
+public class UserInfoHouseBookingViewModel
+{
+    public User user { get; set; }
+    public List<BookingViewModel> bookingList { get; set; }
+}
+
