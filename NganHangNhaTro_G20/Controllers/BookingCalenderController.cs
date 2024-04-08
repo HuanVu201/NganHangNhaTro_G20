3e2a811ffe059dd481ac6ec6ee9a8ae413b42dd7
VuHuan <vuhuan414@gmail.com>
2024-04-08 13:48:19 +0700

    ud feature manage booking

---
+++ NganHangNhaTro_G20/Controllers/BookingCalenderController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NganHangNhaTro_G20.Models;
+using static NganHangNhaTro_G20.Controllers.AdminController;
+
+namespace NganHangNhaTro_G20.Controllers
+{
+    public class BookingCalenderController : Controller
+    {
+        private readonly ProjectDbContext _context;
+
+        public BookingCalenderController(ProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult BookingCalenderView()
+        {
+            return View();
+        }
+
+
+        //DataTable=====================================================================================
+        [HttpGet]
+        public string GetBookings()
+        {
+            var bookingDetails = (from bc in _context.BookingCalenders
+                                  join u in _context.Users on bc.CustomerId equals u.Id
+                                  join h in _context.Houses on bc.HouseId equals h.Id
+                                  select new
+                                  {
+                                      bookingId = bc.Id,
+                                      houseType = h.HouseType,
+                                      housePrice = h.Price,
+                                      customerId = u.Id,
+                                      customerName = u.Name,
+                                      customerPhone = u.PhoneNumber,
+                                  }).ToList();
+            var value = JsonConvert.SerializeObject(new { data = bookingDetails });
+            return value;
+        }
+
+
+        //Chi tiết=====================================================================================
+        [HttpGet]
+        public JsonResult ChiTiet(Guid bookingCalendersId)
+        {
+            var bookingDetails = (from bc in _context.BookingCalenders
+                                  join u in _context.Users on bc.CustomerId equals u.Id
+                                  join h in _context.Houses on bc.HouseId equals h.Id
+                                  join l in _context.Locations on h.OfLocationId equals l.Id
+                                  where bc.Id == bookingCalendersId
+                                  select new
+                                  {
+                                      bookingId = bc.Id,
+                                      bookingNote = bc.Note,
+
+                                      customerName = u.Name,
+                                      customerPhone = u.PhoneNumber,
+                                      customerEmail = u.Email,
+
+                                      houseTitle = h.HouseTitle,
+                                      ownerName = h.OwnerName,
+                                      ownerPhone = h.OwnerPhone,
+                                      address = h.Address,
+                                      acreage = h.Acreage,
+                                      price = h.Price,
+                                      description = h.Desciption,
+                                      houseType = h.HouseType,
+
+                                      locationName = l.Name
+
+                                  }).ToList();
+
+            return Json(bookingDetails);
+        }
+
+        // Xóa các phòng đang chờ xem (Hồ sơ cá nhân)=====================================================================================================
+        [HttpPost]
+        public async Task<int> RemoveBookingHouse(Guid bookingCalendersId, Guid customerId)
+        {
+            var result = -1;
+            var user = await _context.Users.FindAsync(customerId);
+            if (user != null)
+            {
+                string listBooking = user.BookingHouse;
+                listBooking = listBooking.Replace($"{bookingCalendersId};", "");
+                user.BookingHouse = listBooking;
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+        // Xóa=====================================================================================================
+        [HttpPost]
+        public async Task<int> DeleteConfirmed(Guid bookingCalendersId)
+        {
+            var result = -1;
+            var bc = await _context.BookingCalenders.FindAsync(bookingCalendersId);
+            if (bc != null)
+            {
+                _context.BookingCalenders.Remove(bc);
+                await _context.SaveChangesAsync();
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ NganHangNhaTro_G20/wwwroot/js/bookingCalender.js
@@ -0,0 +1,201 @@
+﻿$(document).ready(function () {
+    console.log('BookingCalenderView')
+
+    //=====================================================================================================
+    //LOAD DATATABLES
+
+    $('.contentBlock__content2').html('');
+
+    var html = '';
+    html +=
+        "<table id='table' class='display'>"
+        + "<thead>"
+        + "<tr>"
+        + "<th class='stt'>STT</th>"
+        + "<th class='owner''>Loại phòng</th>"
+        + "<th class='price''>Giá</th>"
+        + "<th class='description'>Người chờ xem</th>"
+        + "<th class='status'>Số điện thoại</th>"
+        + "<th class='action'>Thao tác</th>"
+        + "</tr>"
+        + "</thead>"
+        + "<tbody>"
+
+        + "</tbody>"
+        + "</table>";
+    $('.contentBlock__content2').append(html);
+
+    $("#table").dataTable({
+        processing: true,
+        serverSide: false,
+        filter: true,
+        orderMulti: true,
+        language: {
+            info: "",
+            emptyTable: "Không có dữ liệu!",
+            paginate: {
+                previous: "Trang trước",
+                next: "Trang sau",
+                first: "Trang đầu",
+                last: "Trang cuối",
+                search: "Tìm kiếm:"
+            },
+        },
+
+        ajax: {
+            url: "/BookingCalender/GetBookings",
+            type: "GET",
+            datatype: "json"
+        },
+
+        columns: [
+            {
+                data: null, render: function (data, type, row, meta) {
+                    return meta.row + 1;
+                }
+            },
+            {
+                data: null, render: function (data, type, row, meta) {
+                    return "<span data-toggle='modal'  class='dataRow'>" + data.houseType + "</span>";
+                }
+            },
+
+            {
+                data: null, render: function (data, type, row, meta) {
+                    return "<span data-toggle='modal' class='dataRow'>" + data.housePrice + "</span>";
+                }
+
+            },
+            {
+                data: null, render: function (data, type, row, meta) {
+                    return "<span data-toggle='modal'  class='dataRow'>" + data.customerName + "</span>";
+                }
+
+            },
+            {
+                data: null, render: function (data, type, row, meta) {
+                    return "<span data-toggle='modal'  class='dataRow'>" + data.customerPhone + "</span>";
+                }
+
+            },
+            {
+                data: null, render: function (data, type, row, meta) {
+                    console.log(data.customerId)
+                    return "<a href='' id='btnEdit' idBookingCalender='" + data.bookingId + "' idCustomer='" + data.customerId + "' style='margin: 10px;cursor: pointer;'><i class='fas fa-external-link-alt'></i></a>";
+                }
+            },
+
+        ]
+    });
+
+    //=====================================================================================================
+    //MODAL EDIT
+    $(document).on('click', '#btnEdit', function (e) {
+
+        e.preventDefault();
+
+        var requestIdBooking = $(this).attr('idBookingCalender');
+        var requestIdCustomer = $(this).attr('idCustomer');
+
+
+        const callAPIDetail = async () => {
+            try {
+
+                $.ajax({
+                    url: '/BookingCalender/ChiTiet',
+                    type: 'GET',
+                    data: { bookingCalendersId: requestIdBooking },
+                    success: function (data) {
+                        $('#myModalCRUD').modal('show');
+                        $('.modal-title').html('Cập nhật đặt phòng');
+                        $('.modal-footer').html(
+                            "<button type='button' class='btn btn-primary' id='btnUpdate'>Đã xử lý</button>"
+                            + "<button type = 'button' class='btn btn-default' data-dismiss='modal'>Đóng</button >"
+                        );
+
+                        $('#CustomerName').val(data[0].customerName);
+                        $('#CustomerPhone').val(data[0].customerPhone);
+                        $('#CustomerEmail').val(data[0].customerEmail);
+                        $('#Note').val(data[0].bookingNote);
+                        $('#OwnerName').val(data[0].ownerName);
+                        $('#OwnerPhone').val(data[0].ownerPhone);
+                        $('#HouseTitle').val(data[0].houseTitle);
+                        $('#LocationName').val(data[0].locationName);
+                        $('#Address').val(data[0].address);
+                        $('#Acreage').val(data[0].acreage);
+                        $('#Price').val(data[0].price);
+                        $('#Desciption').val(data[0].description);
+                        $('#HouseType').val(data[0].houseType);
+
+                        $('#CustomerName').attr("readonly", true);
+                        $('#CustomerPhone').attr("readonly", true);
+                        $('#CustomerEmail').attr("readonly", true);
+                        $('#Note').attr("readonly", true);
+                        $('#OwnerName').attr("readonly", true);
+                        $('#OwnerPhone').attr("readonly", true);
+                        $('#HouseTitle').attr("readonly", true);
+                        $('#LocationName').attr("readonly", true);
+                        $('#Address').attr("readonly", true);
+                        $('#Acreage').attr("readonly", true);
+                        $('#Price').attr("readonly", true);
+                        $('#Desciption').attr("readonly", true);
+                        $('#HouseType').attr("disabled", true);
+                    },
+                    error: function (xhr, status, error) {
+                        console.error("Lỗi khi lấy chi tiết: " + error);
+                    }
+                });
+            }
+            catch (error) {
+                console.log("Error detail: " + error);
+            }
+        }
+        callAPIDetail();
+
+
+
+        $(document).on('click', '#btnUpdate', function (e) {
+            let checkAction = 1;
+            var answer = confirm("Bạn đã xử lý xong lịch đặt phòng này?");
+            if (answer) {
+
+                const callAPIDelete = async () => {
+                    try {
+                        const response = await fetch(`/BookingCalender/RemoveBookingHouse?bookingCalendersId=${requestIdBooking}&customerId=${requestIdCustomer}`, { method: "POST" });
+                        const data = await response.text();
+                        if (checkAction == 1) {
+                            checkAction = data;
+                        }
+                    } catch (error) {
+                        console.log("(RemoveBookingHouse) Lỗi khi cập nhật: " + error);
+                        alert("Xóa thất bại!");
+
+                    }
+
+
+                    try {
+                        const response = await fetch(`/BookingCalender/DeleteConfirmed?bookingCalendersId=${requestIdBooking}`, { method: "POST" });
+                        const data = await response.text();
+                        if (checkAction == 1) {
+                            checkAction = data;
+                        }
+                    } catch (error) {
+                        console.log("(DeleteConfirmed) Lỗi khi cập nhật: " + error);
+
+                    }
+                };
+                callAPIDelete();
+
+
+                if (checkAction == 1) {
+                    $('#myModalCRUD').modal('hide');
+                    alert('Xác nhận xử lý thành công!');
+                }
+                else {
+                    alert("Cập nhật thất bại!");
+                }
+            };
+        });
+    });
+
+});
