ce1a79dd66f93098152a4d1a597f20f497b8e568
Dat Nguyen <ntdat09mister@gmail.com>
2024-04-16 23:25:51 +0700

    test

---
+++ NganHangNhaTro_G20/Controllers/TestAjaxController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NganHangNhaTro_G20.Models;
+
+namespace NganHangNhaTro_G20.Controllers
+{
+    public class TestAjaxController : Controller
+    {
+        private readonly ProjectDbContext _dbContext;
+        public TestAjaxController(ProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetRoles()
+        {
+            var roles = _dbContext.Roles.ToList();
+            return Json(roles);
+        }
+        public IActionResult GetRole(string id)
+        {
+            var role = _dbContext.Roles.FirstOrDefault(r => r.Id == id);
+
+            if (role == null)
+            {
+                return NotFound(); // Role không tồn tại
+            }
+
+            return Json(role);
+        }
+    }
+}
---
+++ NganHangNhaTro_G20/Controllers/TestController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NganHangNhaTro_G20.Models;
+
+namespace NganHangNhaTro_G20.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ProjectDbContext _dbContext;
+        public TestController(ProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var listRoles = _dbContext.Roles.ToList();
+            return View(listRoles);
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var role = _dbContext.Roles.Find(id);
+            return View(role);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Role role)
+        {
+            if (role != null)
+            {
+                _dbContext.Roles.Update(role);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var role = _dbContext.Roles.Find(id);
+            return View(role);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var role = _dbContext.Roles.Find(id);
+            _dbContext.Roles.Remove(role);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Role role)
+        {
+            _dbContext.Roles.Add(role);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
